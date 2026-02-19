8cbd1624c56b8edada70680529307aa81839e032
Om <omchimurkar10@gmail.com>
2026-02-19 17:14:24 +0530

    donw with rag

--- core/engine.py
+++ core/engine.py
@@ -3,15 +3,27 @@ from core.parser import get_base_parser
 from core.prompt import get_prompt
 from core.features import compute_features
 
+from rag.retriever import get_retriever
+
 model = get_model()
 base_parser = get_base_parser()
 format_instructions = base_parser.get_format_instructions()
 prompt = get_prompt(format_instructions)
 
+retriever = get_retriever()
+
 chain = prompt | model | base_parser
 
 def analyze_company(company_name: str):
-    intel = chain.invoke({"company_name": company_name})
+    docs = retriever.invoke(company_name)
+
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    intel = chain.invoke({
+        "company_name": company_name,
+        "context": context
+        })
+        
     features = compute_features(intel)
 
     return intel, features
